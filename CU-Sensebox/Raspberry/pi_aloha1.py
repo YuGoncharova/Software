bbec4c00123eadbaaf6468657ad36d8f07671c28
YuGoncharova <63481160+YuGoncharova@users.noreply.github.com>
2020-04-18 20:06:31 +0100

    Update pi_aloha1.py

--- CU-Sensebox/Raspberry/pi_aloha1.py
+++ CU-Sensebox/Raspberry/pi_aloha1.py
@@ -1,28 +1,23 @@
 from raspi_lora import LoRa, ModemConfig
+import random
 import time
-global msg
 global ack
+
 # This is our callback function that runs when a message is received
-def on_recv(payload): #como é que função sabe se foi recebida mensagem?
-    global msg
+def on_recv(payload):
     print("From:", payload.header_from)
     print("Received:", payload.message)
-    print("RSSI: {}; SNR: {}".format(payload.rssi, payload.snr)) # desnecessário?
     msg = payload.message
     if msg == "ack"
         ack = 1
 
-# Use chip select 0. GPIO pin 17 will be used for interrupts
-# The address of this device will be set to 2
+#The selected SPI channel is 0, The selected interrupt pin is GPIO17
 lora = LoRa(0, 17, 2, freq=868, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=False)
 
 lora.on_recv = on_recv
-buffer = []
-buffer.append(msg) # a partir daqui envia para aplicação e base de dados
-
 
 while True:
-    if  flag == 1:
+    if  flag == 1:  #flag will always be activated in the code outside this cycle when there is a message to be sent
       do
           send_to_wait("teste", 255, header_flags=0)
           flag = 0
@@ -32,10 +27,10 @@ while True:
         if ack = 1:
             ack = 0
             break
-        rt = random() # a and b are the time constraints
-        k = 2
+        rt = random.random() #returns a random foating point number from 0 to 1
+        k = #range that we want our random number to be choosen from
         rt1 = k*rt
-        delay(rt1)
+        time.sleep(rt1)
       while ack == 0
 
 # And remember to call this as your program exits...
