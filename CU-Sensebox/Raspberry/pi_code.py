7ff7131ec847ab2d39b16194d761fafcdb607a65
YuGoncharova <63481160+YuGoncharova@users.noreply.github.com>
2020-04-18 19:15:41 +0100

    Update pi_code.py

--- CU-Sensebox/Raspberry/pi_code.py
+++ CU-Sensebox/Raspberry/pi_code.py
@@ -1,19 +1,18 @@
 from raspi_lora import LoRa, ModemConfig
-global msg
+
 # This is our callback function that runs when a message is received
 def on_recv(payload):
-    global msg
     print("From:", payload.header_from)
-    msg = payload.message
+    print("Received:", payload.message)
 
+    
+#The selected SPI channel is 0, The selected interrupt pin is GPIO17
 lora = LoRa(0, 17, 2, freq=868, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True)
 lora.on_recv = on_recv
-buffer = []
-#buffer.append(msg)
 
 while True:
-#    if msg != 0
     print("1")
 
 
+# And remember to call this as your program exits...
 lora.close()
