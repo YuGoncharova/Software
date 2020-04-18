cc8d770e56589bc7355c8df7acc9e8b5bcff41c5
YuGoncharova <63481160+YuGoncharova@users.noreply.github.com>
2020-04-18 19:24:59 +0100

    Update pi_code.py

--- CU-Sensebox/Raspberry/pi_code.py
+++ CU-Sensebox/Raspberry/pi_code.py
@@ -4,7 +4,6 @@ from raspi_lora import LoRa, ModemConfig
 def on_recv(payload):
     print("From:", payload.header_from)
     print("Received:", payload.message)
-
     
 #The selected SPI channel is 0, The selected interrupt pin is GPIO17
 lora = LoRa(0, 17, 2, freq=868, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True)
@@ -12,7 +11,6 @@ lora.on_recv = on_recv
 
 while True:
     print("1")
-
 
 # And remember to call this as your program exits...
 lora.close()
